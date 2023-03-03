46d26210f1497d3f39a0e1c3aadabf39cf49aed1
Antoine Morvan <antoine.morvan@atos.net>
2023-03-03 13:29:39 +0100

    Fix log file name

--- reproduce.sh
+++ reproduce.sh
@@ -8,7 +8,7 @@ COMPILER_NAME=$(echo $COMPILER | cut -d':' -f1)
 COMPILER_VERSION=$(echo $COMPILER | cut -d':' -f2)
 
 # install compiler if not present
-DEST=$(uname -s)/$(uname -m)/${COMPILER_NAME}-${COMPILER_VERSION} ./compilers/${COMPILER_NAME}.sh ${COMPILER_VERSION} |& tee ${COMPILER_NAME}-${COMPILER_VERSION}_$(uname -m)_01-compiler.log
+DEST=$(uname -s)/$(uname -m)/${COMPILER_NAME}-${COMPILER_VERSION} ./compilers/${COMPILER_NAME}.sh ${COMPILER_VERSION} |& tee $(uname -m):${COMPILER_NAME}:${COMPILER_VERSION}:01-compiler.log
 
 # reproduce cmake build & test
-DEST=$(uname -s)/$(uname -m)/openblas_${COMPILER_NAME}-${COMPILER_VERSION} ./openblas-0.3.21.sh ${COMPILER_NAME}-${COMPILER_VERSION} |& tee ${COMPILER_NAME}-${COMPILER_VERSION}_$(uname -m)_02-openblas.log
+DEST=$(uname -s)/$(uname -m)/openblas_${COMPILER_NAME}-${COMPILER_VERSION} ./openblas-0.3.21.sh ${COMPILER_NAME}-${COMPILER_VERSION} |& tee $(uname -m):${COMPILER_NAME}:${COMPILER_VERSION}:02-openblas.log
