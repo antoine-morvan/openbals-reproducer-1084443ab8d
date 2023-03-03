65e723e0a1f215dacfa992f7665fd42519f83853
Antoine Morvan <antoine.morvan@atos.net>
2023-03-03 13:58:56 +0100

    Fix reproduce script

--- reproduce.sh
+++ reproduce.sh
@@ -11,4 +11,4 @@ COMPILER_VERSION=$(echo $COMPILER | cut -d':' -f2)
 DEST=$(uname -s)/$(uname -m)/${COMPILER_NAME}-${COMPILER_VERSION} ./compilers/${COMPILER_NAME}.sh ${COMPILER_VERSION} |& tee $(uname -m):${COMPILER_NAME}:${COMPILER_VERSION}:01-compiler.log
 
 # reproduce cmake build & test
-DEST=$(uname -s)/$(uname -m)/openblas_${COMPILER_NAME}-${COMPILER_VERSION} ./openblas-0.3.21.sh ${COMPILER_NAME}-${COMPILER_VERSION} |& tee $(uname -m):${COMPILER_NAME}:${COMPILER_VERSION}:02-openblas.log
+DEST=$(uname -s)/$(uname -m)/openblas_${COMPILER_NAME}-${COMPILER_VERSION} ./openblas.sh ${COMPILER_NAME}-${COMPILER_VERSION} |& tee $(uname -m):${COMPILER_NAME}:${COMPILER_VERSION}:02-openblas.log
